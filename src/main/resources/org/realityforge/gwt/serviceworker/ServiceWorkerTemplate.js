a0d01a747cef30b0a0fcb16fe1d68df5f57d171c
Peter Donald <peter@realityforge.org>
2020-03-12 10:20:39 +1100

    Immediately move to new service worker on upgrade

--- src/main/resources/org/realityforge/gwt/serviceworker/ServiceWorkerTemplate.js
+++ src/main/resources/org/realityforge/gwt/serviceworker/ServiceWorkerTemplate.js
@@ -4,6 +4,8 @@ var filesToCache = [__RESOURCES__];
 
 self.addEventListener('install', function(e) {
   console.log(moduleName, '[ServiceWorker] Install');
+  // Immediately replace an existing serviceworker with the current service worker
+  self.skipWaiting();
   // Claim all existing clients that would be managed by this service worker so
   // that even the first time loading that they will go through the caching
   clients.claim();
